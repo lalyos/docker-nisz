a427eba4d5d73764c9cb2d3e4124af49c69c2239
lalyos <lalyos@yahoo.com>
2025-03-25 11:24:43 +0100

    sql added

---
+++ sql/init.sql
@@ -0,0 +1,6 @@
+create table food (name varchar(20), price integer, cal integer);
+insert into food VALUES ('pizza', 4000, 3000);
+insert into food VALUES ('pacal', 2000, 2000);
+insert into food VALUES ('giros tal', 4500, 4000);
+insert into food VALUES ('lecso', 1000, 1000);
+insert into food VALUES ('salata', 1900, 900);
